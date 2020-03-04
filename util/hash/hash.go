bef530dea4e38a70d6894a49b42e1e5fd40882b6
hactrox <hactrox@gmail.com>
2020-03-04 17:27:43 +0800

    feature(util): add new package 'hash'

---
+++ util/hash/hash.go
@@ -0,0 +1,56 @@
+package hash
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"neo3-squirrel/util/byteutil"
+
+	"golang.org/x/crypto/ripemd160"
+)
+
+// Hash160 returns hash160 of input data bytes.
+func Hash160(data []byte) []byte {
+	return Ripemd160(Sha256(data))
+}
+
+// Hash256 returns hash256 of input data bytes.
+func Hash256(data []byte) []byte {
+	return Sha256(Sha256(data))
+}
+
+// Sha256 returns sha256 of input data bytes.
+func Sha256(data []byte) []byte {
+	sha256H := sha256.New()
+	sha256H.Reset()
+	sha256H.Write(data)
+	return sha256H.Sum(nil)
+}
+
+// Ripemd160 returns RIPEMD-160 hash bytes.
+func Ripemd160(data []byte) []byte {
+	ripemd160H := ripemd160.New()
+	ripemd160H.Reset()
+	ripemd160H.Write(data)
+	return ripemd160H.Sum(nil)
+}
+
+// GetScriptHash returns hash(160 bits) of input data bytes.
+func GetScriptHash(data []byte) []byte {
+	return Hash160(data)
+}
+
+// GetAssetIDFromScriptHash returns assetID from script hash.
+func GetAssetIDFromScriptHash(scriptHash []byte) string {
+	assetID := hex.EncodeToString(byteutil.ReverseBytes(scriptHash))
+	return assetID
+}
+
+// GetScriptHashFromAssetID returns script hash from assetID.
+func GetScriptHashFromAssetID(assetID string) ([]byte, error) {
+	scriptHash, err := hex.DecodeString(assetID)
+	if err != nil {
+		return nil, err
+	}
+
+	return byteutil.ReverseBytes(scriptHash), nil
+}
---
+++ util/hash/hash_test.go
@@ -0,0 +1,66 @@
+package hash
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHash160(t *testing.T) {
+	data := []byte{0x00, 0x01, 0x02, 0x03}
+	want := "3c3fa3d4adcaf8f52d5b1843975e122548269937"
+	get := Hash160(data)
+
+	if fmt.Sprintf("%x", get) != want {
+		t.Fatalf("Get: %x, want: %s", get, want)
+	}
+}
+
+func TestHash256(t *testing.T) {
+	data := []byte{0x00, 0x01, 0x02, 0x03}
+	want := "f7a355c00c89a08c80636bed35556a210b51786f6803a494f28fc5ba05959fc2"
+	get := Hash256(data)
+
+	if fmt.Sprintf("%x", get) != want {
+		t.Fatalf("Get: %x, want: %s", get, want)
+	}
+}
+
+func TestGetScriptHash(t *testing.T) {
+	data := []byte{0x00, 0x01, 0x02, 0x03}
+	want := "3c3fa3d4adcaf8f52d5b1843975e122548269937"
+	get := GetScriptHash(data)
+
+	if fmt.Sprintf("%x", get) != want {
+		t.Fatalf("Get: %x, want: %s", get, want)
+	}
+}
+
+func TestGetAssetIDFromScriptHash(t *testing.T) {
+	data := []byte{0x00, 0x01, 0x02, 0x03}
+	want := "3033303230313030"
+	get := GetAssetIDFromScriptHash(data)
+
+	if fmt.Sprintf("%x", get) != want {
+		t.Fatalf("Get: %x, want: %s", get, want)
+	}
+}
+
+func TestGetScriptHashFromAssetID(t *testing.T) {
+	assetID := "43cf98eddbe047e198a3e5d57006311442a0ca15"
+	want := "15caa04214310670d5e5a398e147e0dbed98cf43"
+	get, err := GetScriptHashFromAssetID(assetID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if fmt.Sprintf("%x", get) != want {
+		t.Fatalf("Get: %x, want: %s", get, want)
+	}
+}
+
+func TestGetScriptHashFromAssetIDPanic(t *testing.T) {
+	_, err := GetScriptHashFromAssetID("squirrel")
+	if err == nil {
+		t.Fatalf("Get error=nil, want an error")
+	}
+}
