bb7ffbc50e35f1c5e09e19772aa51cde84ce288a
hactrox <hactrox@gmail.com>
2020-09-07 14:52:36 +0800

    feature: enable tasks

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -1,10 +1,9 @@
 package tasks
 
 import (
-	"neo3-squirrel/db"
 	"neo3-squirrel/rpc"
+	"neo3-squirrel/tasks/applog"
 	"neo3-squirrel/tasks/block"
-	"neo3-squirrel/util/color"
 	"neo3-squirrel/util/log"
 )
 
@@ -12,17 +11,8 @@ import (
 func Run() {
 	log.Info("Start Neo3 blockchain data parser.")
 
-	initTask()
-
-	block.StartBlockSyncTask()
-}
-
-func initTask() {
 	rpc.TraceBestHeight()
 
-	lastBlockHeight := db.GetLastBlockHeight()
-	bestBlockHeight := rpc.GetBestHeight()
-
-	log.Info(color.BGreenf("Block sync progress: %d/%d, %d blocks behind",
-		lastBlockHeight, bestBlockHeight, bestBlockHeight-lastBlockHeight))
+	block.StartBlockSyncTask()
+	applog.StartApplicationLogSyncTask()
 }
