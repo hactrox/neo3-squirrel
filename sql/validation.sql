da466636680909a932d5ed77b602a989633c489b
hactrox <hactrox@gmail.com>
2020-09-10 10:19:07 +0800

    chore: optimize validation sql output

--- sql/validation.sql
+++ sql/validation.sql
@@ -5,6 +5,6 @@ SELECT 'Neo3 Squirrel SQL Validation', 'Result'
 UNION ALL
 SELECT 'check block count', IF((SELECT COUNT(`index`) FROM `block`) - (SELECT `block_index` FROM `counter`) = 1, 'PASS', 'FAIL')
 UNION ALL
-SELECT 'check notification count', IF((SELECT SUM(`notifications`) FROM `applicationlog`) - (SELECT COUNT(`id`) FROM `applicationlog_notification`)=0, 'PASS', 'FAIL')
+SELECT 'check notification count', IF((SELECT IFNULL((SELECT SUM(`notifications`) FROM `applicationlog`), 0)) - (SELECT COUNT(`id`) FROM `applicationlog_notification`)=0, 'PASS', 'FAIL')
 UNION ALL
-SELECT 'check transaction count', IF((SELECT SUM(`txs`) FROM `block`) - (SELECT COUNT(`id`) FROM `transaction`)=0, 'PASS', 'FAIL');
+SELECT 'check transaction count', IF((SELECT IFNULL((SELECT SUM(`txs`) FROM `block`), 0)) - (SELECT COUNT(`id`) FROM `transaction`)=0, 'PASS', 'FAIL');
