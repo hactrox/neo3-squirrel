e61b3f512e0336dc8f85ccad194c6eee56006d6f
hactrox <hactrox@gmail.com>
2020-09-11 14:30:57 +0800

    feature: add address cacher

---
+++ cache/address/address.go
@@ -0,0 +1,56 @@
+package address
+
+import (
+	"fmt"
+	"neo3-squirrel/models"
+	"neo3-squirrel/util/log"
+	"sync"
+)
+
+var (
+	// map[address]*models.AddressInfo{}
+	addrInfoMap = map[string]*models.AddressInfo{}
+	mutex       sync.Mutex
+)
+
+// Cache caches address basic info.
+func Cache(address string, blockTime uint64) bool {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	addrInfo, ok := addrInfoMap[address]
+	if !ok {
+		addrInfoMap[address] = &models.AddressInfo{
+			Address:     address,
+			FirstTxTime: blockTime,
+			LastTxTime:  blockTime,
+			Transfers:   1,
+		}
+
+		return true
+	}
+
+	if blockTime > addrInfo.LastTxTime {
+		addrInfo.LastTxTime = blockTime
+	}
+
+	addrInfo.Transfers++
+
+	return false
+}
+
+// Init loads all addr info into cache.
+func Init(array []*models.AddressInfo) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	size := len(addrInfoMap)
+	if size > 0 {
+		err := fmt.Errorf("address info cache can only be loaded once. Current cache size: %d", size)
+		log.Panic(err)
+	}
+
+	for _, addrInfo := range array {
+		addrInfoMap[addrInfo.Address] = addrInfo
+	}
+}
