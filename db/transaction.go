39b58c13341a59bdd4593767892cfd5ddc3b1159
hactrox <hactrox@gmail.com>
2020-10-19 16:02:15 +0800

    improve: optimize transaction db package

--- db/transaction.go
+++ db/transaction.go
@@ -59,6 +59,54 @@ func GetLastTransaction() *models.Transaction {
 	return getTransactionQueryRow(query)
 }
 
+// GetTxCount return the number of txs starts
+// from the given primary key(>=startPK).
+func GetTxCount(startPK uint) uint {
+	query := []string{
+		"SELECT COUNT(`id`)",
+		"FROM `transaction`",
+		fmt.Sprintf("WHERE `id` >= %d", startPK),
+	}
+
+	var count uint
+	err := mysql.QueryRow(mysql.Compose(query), nil, &count)
+	if err != nil {
+		log.Error(mysql.Compose(query))
+		log.Panic(err)
+	}
+
+	return count
+}
+
+// GetTransactions gets transactions starts from the given
+// primary key(>=) and limits to {limit} records from database.
+func GetTransactions(startPK, limit uint) []*models.Transaction {
+	query := []string{
+		fmt.Sprintf("SELECT %s", strings.Join(txColumns, ", ")),
+		"FROM `transaction`",
+		fmt.Sprintf("WHERE `id` >= %d", startPK),
+		fmt.Sprintf("LIMIT %d", limit),
+	}
+
+	return getTransactionsQuery(query)
+}
+
+// GetTransaction returns transaction by txID.
+func GetTransaction(txID string) *models.Transaction {
+	query := []string{
+		fmt.Sprintf("SELECT %s", strings.Join(txColumns, ", ")),
+		"FROM `transaction`",
+		fmt.Sprintf("WHERE `hash` = '%s'", txID),
+		"LIMIT 1",
+	}
+
+	return getTransactionQueryRow(query)
+}
+
+/* ------------------------------
+    DB query result parser
+------------------------------ */
+
 func getTransactionQueryRow(query []string) *models.Transaction {
 	var tx models.Transaction
 	var sysFee string
@@ -93,36 +141,8 @@ func getTransactionQueryRow(query []string) *models.Transaction {
 	return &tx
 }
 
-// GetTxCount return the number of txs starts
-// from the given primary key(>=startPK).
-func GetTxCount(startPK uint) uint {
-	query := []string{
-		"SELECT COUNT(`id`)",
-		"FROM `transaction`",
-		fmt.Sprintf("WHERE `id` >= %d", startPK),
-	}
-
-	var count uint
-	err := mysql.QueryRow(mysql.Compose(query), nil, &count)
-	if err != nil {
-		log.Error(mysql.Compose(query))
-		log.Panic(err)
-	}
-
-	return count
-}
-
-// GetTransactions gets transactions starts from the given
-// primary key(>=) and limits to {limit} records from database.
-func GetTransactions(startPK, limit uint) []*models.Transaction {
-	query := []string{
-		fmt.Sprintf("SELECT %s", strings.Join(txColumns, ", ")),
-		"FROM `transaction`",
-		"WHERE `id` >= ?",
-		"LIMIT ?",
-	}
-
-	rows, err := mysql.Query(mysql.Compose(query), startPK, limit)
+func getTransactionsQuery(query []string) []*models.Transaction {
+	rows, err := mysql.Query(mysql.Compose(query))
 	if err != nil {
 		log.Error(mysql.Compose(query))
 		log.Panic(err)
@@ -162,46 +182,4 @@ func GetTransactions(startPK, limit uint) []*models.Transaction {
 	}
 
 	return txs
-}
-
-// GetTransaction returns transaction by txID.
-func GetTransaction(txID string) *models.Transaction {
-	query := []string{
-		fmt.Sprintf("SELECT %s", strings.Join(txColumns, ", ")),
-		"FROM `transaction`",
-		fmt.Sprintf("WHERE `hash` = '%s'", txID),
-		"LIMIT 1",
-	}
-
-	tx := models.Transaction{}
-	var sysFee string
-	var netFee string
-
-	err := mysql.QueryRow(mysql.Compose(query), nil,
-		&tx.ID,
-		&tx.BlockIndex,
-		&tx.BlockTime,
-		&tx.Hash,
-		&tx.Size,
-		&tx.Version,
-		&tx.Nonce,
-		&tx.Sender,
-		&sysFee,
-		&netFee,
-		&tx.ValidUntilBlock,
-		&tx.Script,
-	)
-	if err != nil {
-		if mysql.IsRecordNotFoundError(err) {
-			return nil
-		}
-
-		log.Error(mysql.Compose(query))
-		log.Panic(err)
-	}
-
-	tx.SysFee = convert.ToDecimal(sysFee)
-	tx.NetFee = convert.ToDecimal(netFee)
-
-	return &tx
 }
