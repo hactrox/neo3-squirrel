2c76c9b3f2d938b2ab795cf5c3b604686e109ac6
hactrox <hactrox@gmail.com>
2020-12-29 19:06:39 +0800

    perf: remove unused code

--- rpc/committee.go
+++
@@ -1,8 +0,0 @@
-package rpc
-
-// GetCommittee returns committee from fullnode.
-func GetCommittee(minBlockIndex uint) *InvokeFunctionResult {
-	method := "getCommittee"
-	contract := "0xde5f57d430d3dece511cf975a8d37848cb9e0525"
-	return InvokeFunction(minBlockIndex, contract, method, nil)
-}
